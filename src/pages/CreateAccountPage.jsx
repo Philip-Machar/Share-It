ae76a762eb9d83f5032e94ca0fd2b93be93750b2
Philip-Machar <philipmachar788@gmail.com>
2024-12-21 11:03:20 +0300

    Initial commit

--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -1,39 +1,42 @@
 const CreateAccountPage = () => {
   return (
-    <div className="w-screen h-screen bg-[#FAFAFA] flex justify-center items-center">
-      <div className="bg-white">
-        <div>
+    <div className="w-screen h-screen bg-white md:bg-[#FAFAFA] flex justify-center items-center p-8">
+      <div>
+        <div className="flex items-center gap-4">
             <img src="/images/logo.png" alt="logo" />
             <h1>Share It</h1>
         </div>
-        <div>
+        <div className="md:bg-white">
             <div>
                 <h2>Create account</h2>
                 <p>let's get you started sharing your links!</p>
             </div>
-            <form>
-                <div>
+            <form className="">
+                <div className="flex flex-col">
                     <label htmlFor="email">Email address</label>
                     <input 
                         id="email"
                         type="email"
                         name="email"
+                        className="border border-black"
                     />
                 </div>
-                <div>
+                <div className="flex flex-col">
                     <label htmlFor="password">Create password</label>
                     <input 
                         id="password"
                         type="password"
                         name="password"
+                        className="border border-black"
                     />
                 </div>
-                <div>
+                <div className="flex flex-col">
                     <label htmlFor="confirmPassword">Confirm password</label>
                     <input
                         id="confirmPassword"
                         type="password"
                         name="confirmPassword"
+                        className="border border-black"
                     />
                 </div>
                 <p>Password must contain at least 8 characters</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        instrumentSans: ["Instrument Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
