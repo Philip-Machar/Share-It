86832e98e85312a927204553bcf89d3462c7f9cc
Philip-Machar <philipmachar788@gmail.com>
2024-12-23 14:26:59 +0300

    Complete navbar mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,33 @@ import CreateAccountPage from "./pages/CreateAccountPage";
 import LoginPage from "./pages/LoginPage";
 import PreviewPage from "./pages/PreviewPage";
 import LinksPage from "./pages/LinksPage";
-import ProfilePage from "./pages/ProfilePage"
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  const routes = [
-    { path: "/", element: <PreviewPage />},
-    { path: "/login", element: <LoginPage />},
-    { path: "/create-account", element: <CreateAccountPage />},
-    {
-      element: <MainLayout />,
-      children: [
-        { path: "/links", element: <LinksPage /> },
-        { path: "/profile", element: <ProfilePage />}
-      ]
-    }
-  ]
+  // const routes = [
+  //   { path: "/", element: <PreviewPage />},
+  //   { path: "/login", element: <LoginPage />},
+  //   { path: "/create-account", element: <CreateAccountPage />},
+  //   {
+  //     element: <MainLayout />,
+  //     children: [
+  //       { path: "/links", element: <LinksPage /> },
+  //       { path: "/profile", element: <ProfilePage />}
+  //     ]
+  //   }
+  // ]
 
-  const routing = useRoutes(routes);
+  // const routing = useRoutes(routes);
 
-  return routing;
-  // return (
-  //   <div>
-  //     <CreateAccountPage />
-  //     {/* <LoginPage /> */}
-  //   </div>
-  // )
+  // return routing;
+  return (
+    <div className="bg-[#FAFAFA] w-screen h-screen">
+      {/* <CreateAccountPage /> */}
+      {/* <LoginPage /> */}
+      <Navbar />
+    </div>
+  )
 };
 
 export default App;
