bc7672dde733b37d0b0cf6234797d4e3e08782a7
Philip-Machar <philipmachar788@gmail.com>
2024-12-28 13:17:10 +0300

    Initial commit

--- src/pages/LinksPage.jsx
+++ src/pages/LinksPage.jsx
@@ -1,3 +1,5 @@
+import { TbBrandGithubFilled } from "react-icons/tb";
+
 const LinksPage = () => {
   return (
     <div className="bg-white text-[#737373] m-5 py-4 px-5 rounded-lg">
@@ -19,15 +21,20 @@ const LinksPage = () => {
             <div>Remove</div>
           </div>
           <form>
-            <div className="flex flex-col items-start mb-4">
-              <label htmlFor="options" className="block text-sm font-medium text-gray-700 mb-1">
+            <div className="flex flex-col items-start mb-4 relative">
+              <label htmlFor="options" className="block text-sm font-medium mb-1">
                 Platform
               </label>
               <select
                 id="options"
-                className="block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#633CFF] focus:border-[#633CFF] sm:text-sm"
+                className="block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#633CFF] focus:border-[#633CFF]"
               >
-                <option value="">Select an option</option>
+                <option value="">
+                  <span className="flex items-center">
+                    <TbBrandGithubFilled className="mr-2" />
+                    GitHub
+                  </span>
+                </option>
                 <option value="option1">Option 1</option>
                 <option value="option2">Option 2</option>
                 <option value="option3">Option 3</option>
