76e85db195d2a4a5c30e016e3aba4f46961bc969
Philip-Machar <philipmachar788@gmail.com>
2024-12-29 21:18:02 +0300

    Initial commit

--- src/pages/LinksPage.jsx
+++ src/pages/LinksPage.jsx
@@ -47,7 +47,7 @@ const LinksPage = () => {
                 name="link" 
                 type="text" 
                 placeholder="eg. https://github.com/Philip-Machar"
-                className="text-[#333333] border border-[#D9D9D9] outline-none py-3 px-4 rounded-lg placeholder-[#737373] w-full"
+                className="text-[#333333] border border-[#D9D9D9] outline-none py-3 pl-4 rounded-lg placeholder-[#737373] w-full"
               />
             </div>
           </form>
